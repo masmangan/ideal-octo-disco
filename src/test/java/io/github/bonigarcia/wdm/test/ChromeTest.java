d3d83638ee0c2794a575d4f48381602b7f778ce0
17104931 <17104931@w1975317.portoalegre.pucrsnet.br>
2019-11-11 23:00:57 -0200

    lauraa qaaaa

--- src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
@@ -61,18 +61,18 @@ public class ChromeTest {
 
     @Test
     public void test() {
-        // Your test code here. For example:
         WebDriverWait wait = new WebDriverWait(driver, 30);
-        driver.get("https://en.wikipedia.org/wiki/Main_Page");
-        By searchInput = By.id("searchInput");
-        wait.until(presenceOfElementLocated(searchInput));
-        driver.findElement(searchInput).sendKeys("Software");
-        By searchButton = By.id("searchButton");
-        wait.until(elementToBeClickable(searchButton));
-        driver.findElement(searchButton).click();
-
+        driver.get("http://10.32.161.142:8080/owners/5");
+        driver.findElement(By.xpath("//a[@href=\"5/pets/new\"]")).click();
+       // wait.until(presenceOfElementLocated(searchInput));
+        //driver.findElement(searchInput).sendKeys("Software");
+       // By searchButton = By.id("searchButton");
+       // wait.until(elementToBeClickable(searchButton));
+       //.findElement(searchButton).click();
+       
         wait.until(textToBePresentInElementLocated(By.tagName("body"),
-                "Computer software"));
+                "New\nPet"));
+        
     }
 
 }
