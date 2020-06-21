2219222234e83d775ef754ca7a22c02bc52d4a8d
lucasstd <lucas.std1@gmail.com>
2020-06-21 18:51:52 -0300

    Implementações de testes com selenium

--- src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
@@ -21,6 +21,8 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClick
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
 
+import java.util.Random;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -32,14 +34,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-/**
- * Test with Chrome.
- *
- * @author Boni Garcia (boni.gg@gmail.com)
- * @since 1.0.0
- */
 public class ChromeTest {
-
+	
+	Random random = new Random();
     private WebDriver driver;
 
     @BeforeClass
@@ -61,18 +58,64 @@ public class ChromeTest {
 
     @Test
     public void test() {
-        // Your test code here. For example:
         WebDriverWait wait = new WebDriverWait(driver, 30);
-        driver.get("https://en.wikipedia.org/wiki/Main_Page");
-        By searchInput = By.id("searchInput");
+        driver.get("https://github.com/");
+        // Espera pelo botão de pesquisa e escreve Lucas Staudt
+        By searchInput = By.xpath("/html/body/div[1]/header/div/div[2]/div[2]/div/div/div/form/label/input[1]");
         wait.until(presenceOfElementLocated(searchInput));
-        driver.findElement(searchInput).sendKeys("Software");
-        By searchButton = By.id("searchButton");
+        driver.findElement(searchInput).sendKeys("Lucas Staudt");
+
+        // clica em pesquisar
+        By searchButton = By.xpath("/html/body/div[1]/header/div/div[2]/div[2]/div/div/div/form/label/div");
         wait.until(elementToBeClickable(searchButton));
         driver.findElement(searchButton).click();
-
-        wait.until(textToBePresentInElementLocated(By.tagName("body"),
-                "Computer software"));
+        
+        // procura pelo usuário e clica no botão
+        By usersButton = By.xpath("//*[@id=\"js-pjax-container\"]/div/div[2]/nav[1]/a[10]");
+        wait.until(elementToBeClickable(usersButton));
+        driver.findElement(usersButton).click();
+        // espera encontrar o link para o perfil lucasstd
+        wait.until(textToBePresentInElementLocated(By.className("mr-1"), "Lucas"));
     }
 
+    public String generateName() {
+    	String randomNumberString = "" + random.nextInt(99999);
+    	return "ewopekokPODa" + randomNumberString;
+    }
+    
+    public String generateEmail() {
+    	String randomNumberString = "" + random.nextInt(99999);
+    	return "ewopekokPODa" + randomNumberString + "@gmail.com";
+    }
+
+    @Test
+    public void testCadastro() {
+    	String name = generateName();
+    	String email = generateEmail();
+    	
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        driver.get("https://angular.realworld.io");
+        
+        By singUpInput = By.xpath("/html/body/app-root/app-layout-header/nav/div/ul/li[3]/a");
+    	wait.until(presenceOfElementLocated(singUpInput));
+    	driver.findElement(singUpInput).click();
+        
+    	By userInput = By.xpath("/html/body/app-root/app-auth-page/div/div/div/div/form/fieldset/fieldset[1]/input");
+    	By emailInput = By.xpath("/html/body/app-root/app-auth-page/div/div/div/div/form/fieldset/fieldset[2]/input");
+    	By passwordInput = By.xpath("/html/body/app-root/app-auth-page/div/div/div/div/form/fieldset/fieldset[3]/input");
+    	
+    	wait.until(presenceOfElementLocated(userInput));
+    	driver.findElement(userInput).sendKeys(name);
+    	
+    	wait.until(presenceOfElementLocated(emailInput));
+    	driver.findElement(emailInput).sendKeys(email);
+    	
+    	wait.until(presenceOfElementLocated(passwordInput));
+    	driver.findElement(passwordInput).sendKeys("312312321");
+    	
+    	By submitButton = By.xpath("/html/body/app-root/app-auth-page/div/div/div/div/form/fieldset/button");
+    	driver.findElement(submitButton).click();
+    	
+    	wait.until(textToBePresentInElementLocated(By.tagName("p"), "Popular Tags"));
+    }
 }
