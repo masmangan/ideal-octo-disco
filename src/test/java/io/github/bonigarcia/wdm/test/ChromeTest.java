d47ccec050acada2afee08939078d6fba4e37976
Mario Specht <ms.specht@gmail.com>
2020-06-23 16:48:59 -0300

    Criado teste para o pestana leilao

--- src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
@@ -17,6 +17,7 @@
 
 package io.github.bonigarcia.wdm.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
@@ -28,9 +29,12 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Test with Chrome.
@@ -59,20 +63,49 @@ public class ChromeTest {
         }
     }
 
+//    @Test
+//    public void test() {
+//        // Your test code here. For example:
+//        WebDriverWait wait = new WebDriverWait(driver, 30);
+//        driver.get("https://en.wikipedia.org/wiki/Main_Page");
+//        By searchInput = By.id("searchInput");
+//        wait.until(presenceOfElementLocated(searchInput));
+//        driver.findElement(searchInput).sendKeys("Software");
+//        By searchButton = By.id("searchButton");
+//        wait.until(elementToBeClickable(searchButton));
+//        driver.findElement(searchButton).click();
+//
+//        wait.until(textToBePresentInElementLocated(By.tagName("body"),
+//                "Computer software"));
+//    }
+
     @Test
-    public void test() {
-        // Your test code here. For example:
+    public void testSearchCourse() throws InterruptedException {
         WebDriverWait wait = new WebDriverWait(driver, 30);
-        driver.get("https://en.wikipedia.org/wiki/Main_Page");
-        By searchInput = By.id("searchInput");
-        wait.until(presenceOfElementLocated(searchInput));
-        driver.findElement(searchInput).sendKeys("Software");
-        By searchButton = By.id("searchButton");
-        wait.until(elementToBeClickable(searchButton));
-        driver.findElement(searchButton).click();
+        driver.get("https://www.leiloes.com.br");
+        By closePopUp = By.id("botaoFecharBanner");
+        Thread.sleep(10000);
+        wait.until(elementToBeClickable(closePopUp));
+        driver.findElement(closePopUp).click();
 
-        wait.until(textToBePresentInElementLocated(By.tagName("body"),
-                "Computer software"));
+        By buttonVeiculo = By.xpath("//button[contains(.,'Veículos')]");
+        wait.until(elementToBeClickable(buttonVeiculo));
+        driver.findElement(buttonVeiculo).click();
+
+        Thread.sleep(10000);
+//        Select dropdown = new Select(driver.findElement(By.tagName("select")));
+
+
+//        By filtro = By.id("botaoFiltrar");
+//        Thread.sleep(5000);
+//        wait.until(elementToBeClickable(closePopUp));
+//        driver.findElement(closePopUp).click();
+
+//        By searchInputSearchCourse = By.id("s");
+//        wait.until(presenceOfElementLocated(searchInputSearchCourse));
+//        driver.findElement(searchInputSearchCourse).sendKeys("Sistemas de informação");
+//
+//        wait.until(textToBePresentInElementLocated(By.tagName("body"),
+//                "Nenhum curso encontrado no momento, veja os cursos com inscrições encerradas e manifeste seu interesse."));
     }
-
 }
