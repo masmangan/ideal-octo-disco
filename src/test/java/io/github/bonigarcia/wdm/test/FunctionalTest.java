7219c736c7463c60e19f5ef5f3842d73271c49ab
Bruno Teixeira <bruno.teixeira92@edu.pucrs.br>
2020-12-06 21:24:03 -0300

    Changing versions of libraries to latest

--- src/test/java/io/github/bonigarcia/wdm/test/FunctionalTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/FunctionalTest.java
@@ -1,29 +1,30 @@
 package io.github.bonigarcia.wdm.test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public abstract class FunctionalTest {
 
     public WebDriver driver;
 
-    @Before
+    @BeforeEach
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         this.driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://web2.0calc.com/");
     }
 
-    @After
+    @AfterEach
     public void tearDown(){
         driver.manage().deleteAllCookies();
         driver.close();
