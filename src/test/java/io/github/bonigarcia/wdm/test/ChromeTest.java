190915561dec6f97bee39983bc68c2e8d5f5f1b3
Bernardo <bernardojandrey@gmail.com>
2021-06-27 04:34:22 -0300

    commit SignUp

--- src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
@@ -31,6 +31,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.concurrent.TimeUnit;
 
@@ -80,6 +81,21 @@ public class ChromeTest {
     */
 
     @Test
+    public void testCarraoNoOlx()  {
+        WebDriverWait wait = new WebDriverWait(driver, 50);
+        driver.get("https://olx.com.br/");
+        WebElement elemento = driver.findElement(By.name("q"));
+        elemento.click();
+        By searchInput = By.id("searchtext");
+        wait.until(presenceOfElementLocated(searchInput));
+        driver.findElement(searchInput).sendKeys("R8 5.2 V10", Keys.ENTER);
+        driver.findElement(By.id("ad-list")).findElement(By.partialLinkText("QUATTRO")).click();
+        try{TimeUnit.SECONDS.sleep(5);}catch(InterruptedException ex){Thread.currentThread().interrupt();}
+        //wait.until(presenceOfElementLocated(By.partialLinkText("QUATTRO")));
+    }
+
+
+    @Test
     public  void testSpotfyLogin(){
         WebDriverWait wait = new WebDriverWait(driver, 30);
         driver.manage().window().maximize();
@@ -104,19 +120,39 @@ public class ChromeTest {
                 "Nome de usuário ou senha incorretos."));
     }
 
-    @Test
-    public void testCarraoNoOlx()  {
-        WebDriverWait wait = new WebDriverWait(driver, 50);
-        driver.get("https://olx.com.br/");
-        WebElement elemento = driver.findElement(By.name("q"));
-        elemento.click();
-        By searchInput = By.id("searchtext");
-        wait.until(presenceOfElementLocated(searchInput));
-        driver.findElement(searchInput).sendKeys("R8 5.2 V10", Keys.ENTER);
-        driver.findElement(By.id("ad-list")).findElement(By.partialLinkText("QUATTRO")).click();
-        try{TimeUnit.SECONDS.sleep(5);}catch(InterruptedException ex){Thread.currentThread().interrupt();}
-        //wait.until(presenceOfElementLocated(By.partialLinkText("QUATTRO")));
 
+    @Test
+    public void testSignUpSpotfy()   {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        driver.manage().window().maximize();
+        driver.get("https://www.spotify.com/br/");
+        try{TimeUnit.SECONDS.sleep(2);}catch(InterruptedException ex){Thread.currentThread().interrupt();}
+        By loginMenuItem = By.linkText("Inscrever-se");
+        driver.findElement(loginMenuItem).click();
+
+        By imputEmail = By.id("email");
+        By imputEmail2 = By.id("confirm");
+        By password = By.id("password");
+        By displayName = By.id("displayname");
+
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/main/div[2]/div/div[4]/div/label/span[2]/span")).click();
+        driver.findElement(imputEmail).sendKeys("test@gmail.com");
+        driver.findElement(imputEmail2).sendKeys("test@gmail.com");
+        driver.findElement(password).sendKeys("12345678");
+        driver.findElement(displayName).sendKeys("test01");
+        driver.findElement(By.id("day")).sendKeys("11");
+        Select month = new Select(driver.findElement(By.id("month")));
+        month.selectByValue("10");
+        driver.findElement(By.id("year")).sendKeys("1990");
+        driver.findElement(By.xpath("//label[@for='gender_option_male']")).click();
+        driver.findElement(By.xpath("//label[@for='marketing-opt-checkbox']")).click();
+        driver.findElement(By.xpath("//label[@for='terms-conditions-checkbox']")).click();
+
+        By signUpButton = By.xpath("//*[@id=\"__next\"]/main/div[2]/div/form/div[10]/div/button/div[1]");
+        driver.findElement(signUpButton).click();
+
+        wait.until(textToBePresentInElementLocated(By.tagName("body"),
+                "Confirme que você não é um robô."));
     }
 
 
